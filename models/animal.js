571f0cf6ca3fcbf47c9850490912661c2ebd89f9
David Bogaerts <dbogaerts@gmail.com>
2019-02-09 20:20:12 -0600

    create animal model

---
+++ models/animal.js
@@ -0,0 +1,25 @@
+const mongoose  = require('mongoose');
+const Shelter    = require('./shelter.js');
+
+const animalSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  breed: String,
+  color: String,
+  gender: String,
+  size: String,
+  narrative: String,
+  imageURL: [String],
+  shelter: [Shelter.schema]
+  social: {
+    likes: Number,
+    follows: Number,
+    shares: Number
+  }
+});
+
+
+
+const Animal = mongoose.model('Animal', animalSchema);
+
+module.exports = Animal;
