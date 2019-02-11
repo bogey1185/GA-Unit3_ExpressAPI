9cae338b4af0c51b249bed289e3f25089d5e0f9c
David Bogaerts <dbogaerts@gmail.com>
2019-02-11 16:52:51 -0600

    changes to models

--- models/property.js
+++ models/property.js
@@ -9,8 +9,9 @@ const propertySchema = new mongoose.Schema({
     state: String,
     zipCode: Number
   },
-  displayProperty: Boolean,
-  inviteCode: String,
+  displayProperty: Boolean, //switch to false after LL deletes listing. this allows for saving after LL 'deletes' listing
+  propertyCode: String,
+  readOnly: Boolean,  //this will switch to true once tenant uploads photos. Therefore, can't be changed later.
   landlordImgUrl: String,
   inspectionData: [{
     imageUrl: {type: String, required: true},
