63583e543ae4917d3f3b09919e4c411cdb5f9aaf
David Bogaerts <dbogaerts@gmail.com>
2019-02-13 12:56:17 -0600

    preparing API route for creating new address

--- controllers/properties.js
+++ controllers/properties.js
@@ -1,7 +1,8 @@
 const express   = require('express');
 const router    = express.Router();
-const Property    = require('../models/property.js');
+const Property  = require('../models/property.js');
 const request   = require('superagent');
+const USPS      = require('../extra_express/uspsapikey.js');
 
 //create new property
     //-- address of new property used to auto generate
@@ -10,10 +11,57 @@ const request   = require('superagent');
     //invite code also created by landlord when property is created
     //so it can be saved and send to the tenant
 
+//~~~~~~~~~~~~~~~~~new property~~~~~~~~~~~~~~~~~//
+
+router.post('/new', async (req, res, next) => {
+  try {
+    console.log(req.body, ' NEW PROP REQ BODY');
+
+    //need to sanitize submitted address through USPS API
+    //first build API query based on req body
+    const apiQueryString = `http://production.shippingapis.com/ShippingAPITest.dll?API=Verify&XML=<AddressValidateRequest USERID="${USPS}"><Address ID="0"><Address1>${req.body.unit}</Address1><Address2>${req.body.street}</Address2><City>${req.body.city}</City><State>${req.body.state}</State><Zip5>${req.body.zipCode}</Zip5><Zip4></Zip4></Address></AddressValidateRequest>`
+    console.log(apiQueryString, 'API QUERY STRING');
+    //api request to USPS
+    //make variable to store response for further processing
+    const uspsResponse;
+    //api request
+    request.get(apiQueryString).end((err, data) => {
+      if (err) {
+        res.send(err)
+      } else {
+      //data delivered as XML, so need to convert to JSON
+        res.send(data)
+      }
+
+    });
+    console.log(uspsResponse, 'USPS RESPONSE');
 
 
+    
+        
+  } catch (err) {
+    console.log(err);
+    next(err);
+  
+  }
+})
 
 
+// router.get('/', (req, res) => {
+//   request
+//     .get(`https://api.openweathermap.org/data/2.5/forecast?zip=60610,us${API_KEY_QS}`)
+//     .end((err, data) => {
+//       const pretty = JSON.parse(data.text)
+
+//       // pass this data to a template
+
+
+//       // somehow add something to the db
+
+
+//       res.json(pretty)
+//     })
+// })
 
 
 
--- models/property.js
+++ models/property.js
@@ -1,12 +1,12 @@
 const mongoose  = require('mongoose');
 
 const propertySchema = new mongoose.Schema({
+  ownerUsername: String,
   street: String,
   unit: String,
   city: String, 
   state: String,
   zipCode: Number,
-  displayProperty: Boolean, //switch to false after LL deletes listing. this allows for saving after LL 'deletes' listing
   propertyCode: String,
   readOnly: Boolean,  //this will switch to true once tenant uploads photos. Therefore, can't be changed later.
   inspectionData: [{
